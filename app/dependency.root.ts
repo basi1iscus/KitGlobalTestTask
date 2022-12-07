aa569a95a0065b7ca56846892bc6ea6f07a50057
basi1iscus <vasyl.kaminnyi@gmail.com>
2022-12-07 21:32:13 +0200

    add logger interface

---
+++ app/dependency.root.ts
@@ -0,0 +1,49 @@
+// Mongoose
+import MongooseConnect from './service/mongoose/dbconnect'
+import UserMongooseService from './service/mongoose/user.service'
+import DoctorMongooseService from './service/mongoose/doctor.service'
+import AppointmentMongooseService from './service/mongoose/appointment.service'
+
+import UserController from './controller/user.controller'
+import DoctorController from './controller/doctor.controller'
+import AppointmentsController from './controller/appointments.controller'
+
+import { IUserDBService } from './interface/user.interface'
+import { IDoctorDBService } from './interface/doctor.interface'
+import { IAppointmentDBService } from './interface/appointment.interface'
+import { ILogger } from './interface/logger.interface'
+
+import { FSNotificationService } from './service/notification/fs.notification'
+import { PinoLogger } from './utils/logger'
+
+export let userController: UserController
+export let doctorController: DoctorController
+export let appointmentController: AppointmentsController
+export const log: ILogger = new PinoLogger()
+
+export async function createDependencies(config: any) {
+    let databaseConnect: any
+    let userDBService: IUserDBService
+    let doctorDBService: IDoctorDBService
+    let appointmentDBService: IAppointmentDBService
+
+    switch (config.storageService) {
+        case 'mongoose': {
+            databaseConnect = new MongooseConnect()
+            await databaseConnect.connect(config.mongoUri, config.databaseName)
+            userDBService = new UserMongooseService()
+            userController = new UserController(userDBService)
+            doctorDBService = new DoctorMongooseService()
+            doctorController = new DoctorController(doctorDBService)
+            appointmentDBService = new AppointmentMongooseService()
+            appointmentController = new AppointmentsController(appointmentDBService)
+            break
+        }
+    }
+
+    if (config.notificationPath) {
+        const notification = new FSNotificationService(config.notificationPath)
+        userController.notificationService = notification
+        userController.createNotifications()
+    }
+}
---
+++ app/utils/logger.ts
@@ -0,0 +1,23 @@
+import logger from 'pino'
+
+import { ILogger } from '../interface/logger.interface'
+
+export class PinoLogger implements ILogger {
+    log
+    constructor() {
+        this.log = logger({
+            base: undefined,
+            timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`
+          })
+    }
+    
+    info(message: string): void {
+        this.log.info(message)
+    }
+    error(message: string): void {
+        this.log.error(message)
+    }
+    warning(message: string): void {
+        this.log.warn(message)
+    }
+}
